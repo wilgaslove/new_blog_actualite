e92c6fafbf385b9f9e077250ede383b1843bf9dd
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-29 17:38:46 +0200

    app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-   <NuxtPage />
+  <div class="bg-gray-200 dark:bg-black">
+    
+    <div class="max-w-4xl px-4 mx-auto">
+      <NuxtLayout >
+        <NuxtPage />
+      </NuxtLayout>
+    </div>
   </div>
 </template>
+<style>
+
+</style>
