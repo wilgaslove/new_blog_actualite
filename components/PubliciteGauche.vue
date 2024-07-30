c6bf476bbbf7aa86e21b11c506c639d784ccd7fb
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-30 17:39:03 +0200

    correction de pub sm

--- components/PubliciteGauche.vue
+++ components/PubliciteGauche.vue
@@ -3,7 +3,7 @@
     <div class="relative">
    
     <div class="animate-bounce-slow fixed top-[21%] left-5   text-lg text-black py-5 px-4 shadow-2xl  h-[30%] w-[15%] 
-    sm:w-[25%] sm:h-[17%] sm:px-0 sm:left-1 sm:text-xs sm:pt-0 sm:top-[55%] sm:left-[78%]
+    sm:w-[25%] sm:h-[17%] sm:px-0 sm:text-xs sm:pt-0 sm:top-[55%] sm:left-[75%] 
     md:w-[20%] md:h-[18%] md:px-0 md:left-1 md:text-xs md:pt-0 md:top-[30%]
     lg:w-[15%] lg:h-[20%] lg:px-0 lg:left-1 lg:text-xs lg:pt-0 lg:ml-[5%] lg:top-[30%]
     xl:w-[15%] xl:h-[20%] xl:px-0 xl:left-1 xl:text-xs xl:pt-0 xl:ml-[10%] xl:top-[30%]
@@ -16,28 +16,36 @@
     </div>
    </div>
 
-   <!-- <div class=" fixed  left-5 shadow-2xl 
-    
-    md:w-[20%] md:h-[18%] md:px-0 md:left-1 md:pt-0 md:bottom-[30%]
-    lg:w-[15%] lg:h-[20%] lg:px-0 lg:left-1  lg:pt-0 lg:ml-[5%] lg:bottom-[30%]
-    xl:w-[15%] xl:h-[20%] xl:px-0 xl:left-1 xl:pt-0 xl:ml-[10%] xl:bottom-[3%]
-    2xl:w-[15%] 2xl:h-[20%] 2xl:px-0 2xl:left-1  2xl:pt-0 2xl:ml-[15%] 2xl:bottom-[30%] 2xl:hidden 
-   
+
+   <div class="animate-pulse-slow fixed bottom-[21%] left-5   text-lg text-black py-5 px-4 shadow-2xl  h-[30%] w-[15%] 
+    sm:hidden
+    md:w-[20%] md:h-[10%] md:px-0 md:left-1 md:text-xs md:pt-0 md:bottom-[30%]
+    lg:w-[15%] lg:h-[10%] lg:px-0 lg:left-1 lg:text-xs lg:pt-0 lg:ml-[5%] lg:bottom-[20%]
+    xl:w-[15%] xl:h-[10%] xl:px-0 xl:left-1 xl:text-xs xl:pt-0 xl:ml-[10%] xl:bottom-[20%]
+    2xl:w-[15%] 2xl:h-[20%] 2xl:px-0 2xl:left-1 2xl:text-xs 2xl:pt-0 2xl:ml-[15%] 2xl:bottom-[30%]
    ">
       
-      <div class=" ">
+      <div class="p-1  
+      sm:text-xs sm:px-0 sm:pt-0
+      md:text-xs md:px-0 md:pt-0 
+      lg:text-xs lg:px-0 lg:pt-0 
+      xl:text-xs xl:px-0 xl:pt-0 
+      2xl:text-xs 2xl:px-0 2xl:pt-0  
+      ">
         <a href="/presentation">
           <img src="assets\img\logo.png" alt="" class="object-cover " >
         </a>
         
-        <h1 class="font-bold text-black bg-white text-lg text-black py-5 px-4">
+        <h1 class="font-bold text-black bg-white">
           <a href="/presentation" >
             Qui sommes-nous ?
           </a>
         </h1>
       </div>
       
-    </div> -->
+    </div>
+
+ 
 
   </div>
 </template>
