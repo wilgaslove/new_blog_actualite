db6c85d45f359d29d9704b4b275e922f6adda232
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-30 18:51:30 +0200

    css slug amour

--- pages/amour/[...slug].vue
+++ pages/amour/[...slug].vue
@@ -2,8 +2,8 @@
 <NavBarreComponent />
 
   <div class="mt-[15%]  rounded-2xl
-  sm:mt-[30%] sm:mx-[15%]   sm:pt-4
-  md:mt-[18%] md:mx-[20%]  md:pt-4
+  sm:mt-[30%] sm:mr-[22%]   sm:pt-10
+  md:mt-[18%] md:mx-[20%]  md:pt-10
   lg:mt-[15%] lg:mx-[20%]  lg:pt-4
   xl:mt-[15%] xl:mx-[20%]  xl:pt-4
   2xl:mt-[15%] 2xl:mx-[20%] 2xl:pt-4
