2d61ac73af96de20d12d0301a7ea495a0541f40c
Wilgas ADJOVI <Wilfrieda216@gmail.com>
2024-07-31 13:30:26 +0200

    chita pub

--- pages/pubclient.vue
+++ pages/pubclient.vue
@@ -1,12 +1,15 @@
 <template>
   <NavBarreComponent />
   <div>
+    <img src="assets\img\2.png" alt="image d'accueil Chita Beauty Shop">
     <p>
       Les bijoux ne sont pas seulement des accessoires de mode ; ils sont des symboles de beauté, de luxe et de sophistication. En intégrant des bijoux dans votre garde-robe, vous pouvez transformer une tenue ordinaire en une œuvre d'art. Que ce soit une paire de boucles d'oreilles scintillantes, un collier élégant ou une bague étincelante, chaque pièce de bijou ajoute une touche de glamour et d'exclusivité à votre apparence. Les bijoux ont le pouvoir de rehausser votre style personnel et de vous faire sentir unique et spécial.
     </p>
+    <img src="assets\img\bagRed.jpg" alt="image bague de mariage">
     <p>
       Les bijoux racontent également des histoires et évoquent des émotions profondes. Une bague de fiançailles symbolise l'amour et l'engagement, tandis qu'un collier hérité représente la tradition et la famille. Chaque pièce de bijou porte en elle des souvenirs précieux et des moments inoubliables. En portant des bijoux, vous pouvez exprimer votre individualité et partager des récits personnels qui vous tiennent à cœur. Les bijoux ne sont pas seulement des objets matériels ; ils sont des témoins de votre vie et de vos expériences.
     </p>
+    <img src="assets\img\boite-cadeaux-grande-taille-bijoux.png" alt="">
     <p>
       Les bijoux peuvent également être des cadeaux significatifs. Offrir un bijou à un être cher est un geste de grande valeur et d'affection. Que ce soit pour un anniversaire, un mariage ou une occasion spéciale, les bijoux sont des présents qui marquent les moments importants de la vie. Ils symbolisent l'amour, l'amitié et la reconnaissance, et sont des souvenirs précieux que l'on chérit pour toujours.
     </p>
@@ -22,6 +25,7 @@
     <p>
       Nous choisi, c'est choise la qualité. Et nous somme là pour rendre très ellégant(e)
     </p>
+    <img src="assets\img\1.png" alt="">
   </div>
 </template>
 
@@ -29,6 +33,9 @@
 
 </script>
 
-<style>
+<style scoped>
+img {
+  
+}
 
 </style>
